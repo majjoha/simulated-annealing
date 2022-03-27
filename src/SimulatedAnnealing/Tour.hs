89a74cf01a3e6939439cea510b230f3b55fd7db0
Mathias Jean Johansen <mathias@mjj.io>
2022-03-27 14:14:00 +0200

    Add `totalDistance`.

--- src/SimulatedAnnealing/Tour.hs
+++ src/SimulatedAnnealing/Tour.hs
@@ -1,6 +1,7 @@
 module SimulatedAnnealing.Tour
   ( Tour (..),
     calculateDistanceBetweenCities,
+    totalDistance,
   )
 where
 
@@ -11,3 +12,10 @@ type Tour = [City]
 calculateDistanceBetweenCities :: City -> City -> Float
 calculateDistanceBetweenCities (x1, y1) (x2, y2) =
   sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+
+totalDistance :: Tour -> Float
+totalDistance [] = 0
+totalDistance [(_, _)] = 0
+totalDistance [c1, c2] = calculateDistanceBetweenCities c1 c2
+totalDistance (c1 : c2 : xs) =
+  calculateDistanceBetweenCities c1 c2 + totalDistance (c2 : xs)
--- test/SimulatedAnnealing/TourSpec.hs
+++ test/SimulatedAnnealing/TourSpec.hs
@@ -8,3 +8,14 @@ spec = do
   describe "calculateDistanceBetweenCities" $ do
     it "returns the distance between two cities" $ do
       calculateDistanceBetweenCities (150, 250) (200, 300) `shouldBe` 70.71068
+
+  describe "totalDistance" $ do
+    it "returns 0 when the tour is empty" $ do
+      totalDistance [] `shouldBe` 0
+
+    it "returns 0 when the tour only has one city" $ do
+      totalDistance [(1, 2)] `shouldBe` 0
+
+    it "calculates the distance when the tour has two cities or more" $ do
+      totalDistance [(1, 2), (1, 3), (3, 1), (2, 2), (2, 3)]
+        `shouldBe` 6.2426405
