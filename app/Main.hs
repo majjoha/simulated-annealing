1cb6baca3f489e9c8d1d06349b31d9fb94591645
Mathias Jean Johansen <mathias@mjj.io>
2022-03-27 16:14:46 +0200

    Run algorithm in `Main.hs`.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import           Control.Monad      (replicateM)
+import           SimulatedAnnealing
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  initialTour <- replicateM 1000 generateRandomCity
+  let initialDistance = totalDistance initialTour
+
+  (result, iterations) <- generateShortestTour initialTour initialTemperature 0
+  let finalTourDistance = totalDistance result
+  let difference = initialDistance - finalTourDistance
+
+  putStrLn ("Iterations: " ++ show iterations)
+  putStrLn ("Initial distance: " ++ show initialDistance)
+  putStrLn ("Final tour distance: " ++ show finalTourDistance)
+  putStrLn ("Difference: " ++ show difference)
