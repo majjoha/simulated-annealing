442b3dde99ef7c0f8bbef6d74b2b3620288bd4cc
Mathias Jean Johansen <mathias@mjj.io>
2022-03-27 13:08:42 +0200

    Add `generateRandomCity`.

--- src/SimulatedAnnealing/City.hs
+++ src/SimulatedAnnealing/City.hs
@@ -1,10 +1,19 @@
 module SimulatedAnnealing.City
   ( City (..),
     mkCity,
+    generateRandomCity,
   )
 where
+
+import           System.Random
 
 type City = (Float, Float)
 
 mkCity :: Float -> Float -> City
 mkCity x y = (x, y)
+
+generateRandomCity :: IO City
+generateRandomCity = do
+  x <- randomRIO (1 :: Float, 100)
+  y <- randomRIO (1 :: Float, 100)
+  return (x, y)
