6334e718acd70ae19963fb1d1d199b728f50bd08
Mathias Jean Johansen <mathias@mjj.io>
2022-03-27 13:47:53 +0200

    Add `coolingFactor`.

--- src/SimulatedAnnealing/Algorithm.hs
+++ src/SimulatedAnnealing/Algorithm.hs
@@ -2,8 +2,12 @@
 
 module SimulatedAnnealing.Algorithm
   ( initialTemperature,
+    coolingFactor,
   )
 where
 
 initialTemperature :: Float
 initialTemperature = 10_000
+
+coolingFactor :: Float
+coolingFactor = 0.99995
--- test/SimulatedAnnealing/AlgorithmSpec.hs
+++ test/SimulatedAnnealing/AlgorithmSpec.hs
@@ -10,3 +10,7 @@ spec = do
   describe "initialTemperature" $ do
     it "returns 10000" $ do
       initialTemperature `shouldBe` 10_000
+
+  describe "coolingFactor" $ do
+    it "returns 0.99995" $ do
+      coolingFactor `shouldBe` 0.99995
